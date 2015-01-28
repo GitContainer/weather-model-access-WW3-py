235c622be8d17bd097d3ce5005e81879f43598f3
John Fry <jfry@esri.com>
2015-01-28 14:32:04 -0500

    Edited to change to grab NWW world URL
    -Changed URL to grab another site

--- RetrieveWW3GribData.py
+++ RetrieveWW3GribData.py
@@ -1,12 +1,13 @@
 #-------------------------------------------------------------------------------
-# Name:  Retrieve WW3 MultiGrid Global Data from NOMADS
+# Name:  Retrieve WW3 Model Data from NOAA NOMADS
 # Purpose: Downloads a Grib file From NOAA NOMADS
 # Author:      John Fry
 #               DC Technology Office, Defense Team
 # Created:     12/30/2014
 # Copyright:   (c) john6807 2014
 # Licence:     <your licence>
-# Updated 1/25/15 for multi-grid changes in NOAA's website
+# Updated 1/25/15 for multi-grid changes in NOAA's website.
+# Updated 1/28/15 found smaller dataset to link to
 #-------------------------------------------------------------------------------
 
 #Modules
@@ -26,16 +27,23 @@ downloadfolder = str(os.path.join(scriptPath) + r"\scratch\WW3")
 
 
 #Base Url where GRIB2 data resides
-#urlNWW31 = "http://nomads.ncep.noaa.gov/cgi-bin/filter_wave_multi.pl?file=multi_1.glo_30mext.t00z.f000.grib2&all_lev=on&all_var=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fmulti_1."
-urlNWW31 = "http://nomads.ncep.noaa.gov/pub/data/nccf/com/wave/prod/multi_2."
-urlNWW32= "/multi_2.glo_30m.t00z.grib2"
-#Get UTC Date
+# Original URL urlNWW3 = "http://nomads.ncep.noaa.gov/pub/data/nccf/com/wave/prod/multi_1.20150128/nww3.t00z.grib.grib2"
+# Going to Parse URL string to allow for the date to be inserted
+
+urlNWW31 = "http://nomads.ncep.noaa.gov/pub/data/nccf/com/wave/prod/multi_1."
+urlNWW32= "/nww3.t00z.grib.grib2"
+
+#Get Date/Time- was developed for UTC but found issues on early monring East Coast USA time where model for the day was not yet avail."
+# Writing to get local time of machine
+
 #utctime = str(datetime.datetime.utcnow())
 localtime = str(datetime.datetime.now())
+
 #formatutc = time.strftime('%Y%m%d',time.strptime(utctime,"%Y-%m-%d %H:%M:%S.%f"))
 formatlocal = time.strftime('%Y%m%d',time.strptime(localtime,"%Y-%m-%d %H:%M:%S.%f"))
+
 #dlWW3data = str(downloadfolder + r"\multi_2.glo_30m.t00z." +formatutc+ r".grib.grib2")
-dlWW3data = str(downloadfolder + r"\multi_2.glo_30m.t00z." +formatlocal+ r".grib.grib2")
+dlWW3data = str(downloadfolder + r"\nww3.t00z." +formatlocal+ r".grib.grib2")
 outWW3 = open(dlWW3data, 'wb')
 
 print ("Model Date (UTC)")
@@ -48,6 +56,7 @@ arcpy.AddMessage(formatlocal)
 
 #Combine Strings to form URL
 #dlWW3 = str(urlNWW31 + formatutc + urlNWW32)
+
 dlWW3 = str(urlNWW31 + formatlocal + urlNWW32)
 print (dlWW3)
 arcpy.AddMessage(dlWW3)
